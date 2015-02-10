f0a2078cf0c140d6c5a7eaa4eaa57c60c02e1f68
Krosen11 <larrybarnson@gmail.com>
2015-02-10 17:58:35 -0600

    Added analog dash coordinates to Constants header file

--- ST7735_Constants.h
+++ ST7735_Constants.h
@@ -5,6 +5,9 @@
 static int xVals[60] = {};
 static int yVals[60] = {};
 static int analogNumLocs[12] = {};
+	//These next two arrays go in this order: clockwise from 0 to 12, and dimensions from inside to outside
+static int analogDashX[24] = {64, 64, 93, 96, 115, 119, 122, 127, 115, 119, 93, 96, 64, 64, 35, 32, 13, 9, 5, 0, 13, 9, 35, 32};
+static int analogDashY[24] = {21, 16, 29, 25, 51, 48, 80, 80, 109, 112, 131, 135, 139, 144, 132, 135, 109, 112, 80, 80, 51, 48, 29, 25};
 static int digitalNumLocs[28] = {};
 
 #endif
