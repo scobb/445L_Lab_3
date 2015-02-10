018614991e1bd0204203cc21a72a3901073b223a
Krosen11 <larrybarnson@gmail.com>
2015-02-10 12:12:57 -0600

    Added Constants header file

---
+++ ST7735_Constants.h
@@ -0,0 +1,10 @@
+#ifndef _ST7735_CONSTANTS_H_
+#define _ST7735_CONSTANTS_H_
+#include <stdint.h>
+
+static int xVals[60] = {};
+static int yVals[60] = {};
+static int analogNumLocs[12] = {};
+static int digitalNumLocs[28] = {};
+
+#endif
